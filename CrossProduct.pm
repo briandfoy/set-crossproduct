a2c0eac5215b302820bfe6157b9bd5754048d43c
brian d foy <brian.d.foy@gmail.com>
2007-01-10 05:04:31 +0000

    * updated for copyright and license
    git-svn-id: http://brian-d-foy.svn.sourceforge.net/svnroot/brian-d-foy/trunk@1997 d9f60a02-fc28-0410-9752-bc0d6c4f57a6

--- CrossProduct.pm
+++ CrossProduct.pm
@@ -472,11 +472,12 @@ members of the project can shepherd this module appropriately.
 
 brian d foy, C<< <bdfoy@cpan.org> >>
 
-=head1 COPYRIGHT
+=head1 COPYRIGHT AND LICENSE
 
-Copyright 2001-2006, brian d foy.
+Copyright (c) 2001-2007 brian d foy.  All rights reserved.
 
-This package falls under the same terms as Perl itself.
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
 
 =cut
 
