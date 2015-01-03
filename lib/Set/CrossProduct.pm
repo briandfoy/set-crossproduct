a1ebd4d7d02747b6821f2d6cd45c90e0c6d9d008
brian d foy <brian.d.foy@gmail.com>
2015-01-03 17:05:51 -0500

    Update copyright statement

--- lib/Set/CrossProduct.pm
+++ lib/Set/CrossProduct.pm
@@ -494,7 +494,7 @@ brian d foy, C<< <bdfoy@cpan.org> >>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (c) 2001-2014 brian d foy.  All rights reserved.
+Copyright © 2001-2015, brian d foy <bdfoy@cpan.org>. All rights reserved.
 
 This program is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
