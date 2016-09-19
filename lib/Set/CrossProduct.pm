cc30f3fb25ff3b31eb746facb83bd5290c2c9138
brian d foy <brian.d.foy@gmail.com>
2016-09-19 15:27:37 -0400

    Update copyright date

--- lib/Set/CrossProduct.pm
+++ lib/Set/CrossProduct.pm
@@ -540,7 +540,7 @@ Matt Miller implemented the named sets feature.
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright © 2001-2015, brian d foy <bdfoy@cpan.org>. All rights reserved.
+Copyright © 2001-2016, brian d foy <bdfoy@cpan.org>. All rights reserved.
 
 This program is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
